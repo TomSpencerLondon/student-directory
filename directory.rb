c1f25d2bb878c572a7f4d0bcd4c988b00d964b17
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-09-12 18:40:50 +0100

    extracts code into methods

--- directory.rb
+++ directory.rb
@@ -14,14 +14,23 @@ students = [
   "Norman Bates"
 ]
 
-#and then print them
-
-puts "The students of Villains Academy"
-puts "--------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-#finally, we print the total number of students
+def print(names)
+  names.each do |name|
+    puts name
+  end 
+end
 
-puts "Overall, we have #{students.count} great students"
+def
+
+def print_footer(names)
+ puts "Overall, we have #{students.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
