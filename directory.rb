cb6a6ead5bfed3e055ad217a6bce74b4eb7cfb8e
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-09-16 11:56:03 +0100

    refactors student list

--- directory.rb
+++ directory.rb
@@ -5,33 +5,42 @@ require "pry"
   @user_response = ""
   @user_response_final = ""
 
-def interactive_menu
-    loop do
-  #1. print the menu and ask the user what to do
+def print_menu
+  puts "---------------------------"
+  puts "What would you like to do?"
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit" #9 because we'll be adding more items
+  puts "---------------------------"
+end
 
-  #2. read the input and save it into a variable
-  selection = gets.chomp
-  #3. do what the user has asked
-    case selection
-      when "1"
-        input_students
-        #input the students
-      when "2"
-        print_header
-        print_students_list
-        print_footer
-        #show the students
-      when "9"
-        exit #This will cause the program to terminate"
-      else
-        puts "I don't know what you meant, try again"
-      end
+def show_students
+  print_header
+  print_students_list
+  print_footer
+  #show the students
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+      #input the students
+    when "2"
+      show_students
+    when "9"
+      exit #This will cause the program to terminate"
+    else
+      puts "I don't know what you meant, try again"
     end
   end
 
+def interactive_menu
+  loop do
+  print_menu
+  process(gets.chomp)
+  end
+end
 
 def input_students
   puts "Please enter the names of the students"
