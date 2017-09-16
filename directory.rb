10b6cd3504b1b1ddcfb22a1d02767e4b0bb9b366
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-09-16 11:38:24 +0100

    adds interactive directory

--- directory.rb
+++ directory.rb
@@ -5,6 +5,34 @@ require "pry"
   @user_response = ""
   @user_response_final = ""
 
+def interactive_menu
+    loop do
+  #1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" #9 because we'll be adding more items
+
+  #2. read the input and save it into a variable
+  selection = gets.chomp
+  #3. do what the user has asked
+    case selection
+      when "1"
+        input_students
+        #input the students
+      when "2"
+        print_header
+        print_students_list
+        print_footer
+        #show the students
+      when "9"
+        exit #This will cause the program to terminate"
+      else
+        puts "I don't know what you meant, try again"
+      end
+    end
+  end
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -73,6 +101,10 @@ blinding.pry
 
 end
 
+
+
+
+
 def student_singular(count)
   if count == 1 then "student" else "students" end
 end
@@ -80,7 +112,7 @@ end
 
 def print_header
   puts "The students of Villains Academy" if @students.count > 0
-  puts "-------------" 
+  puts "-------------"
 end
 
 def print_students_list
@@ -171,7 +203,4 @@ def print_footer
  puts "You have entered no students" if @students.count == 0
 end
 
-students = input_students
-print_header
-print_students_list
-print_footer
+interactive_menu
